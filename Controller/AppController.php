59dc27002383a5a4bf2f5e0954cd208fea62f94d
Tomás Solar <tsolar@gmail.com>
2013-08-14 15:45:06 -0400

    Controller/AppController.php: added before filter with scaffold and cookie auth management

--- Controller/AppController.php
+++ Controller/AppController.php
@@ -24,4 +24,48 @@ App::uses('Controller', 'Controller');
  * @link		http://book.cakephp.org/2.0/en/controllers.html#the-app-controller
  */
 class AppController extends Controller {
+	
+	public $components = array(
+		'DebugKit.Toolbar',//=>array('panels'=>array('Redis', 'Tags'), 'forceEnable'=>false),
+		'Session',
+		'Cookie',
+		'Auth' => array(
+			'authenticate'=>array(
+				'Form'=> array(
+					'fields'=>array('username'=>'email', 'password'=>'password'),
+					'passwordHasher' => array(
+						'className' => 'Blowfish',
+					),
+				)
+			),
+			'loginRedirect' => '/',
+			'logoutRedirect' =>'/',
+		)
+	);
+
+	public function beforeFilter() {
+
+		if (Configure::read('debug') > 0) {
+			$this->scaffold = '';
+			$this->Auth->allow('index', 'add');
+		}
+
+		// This allows me to log the user in with the cookie
+		$userData = $this->Cookie->read('User');
+		if (!empty($userData)) {
+			// Loader User model
+			$this->loadModel($this->Auth->authenticate['Form']['userModel']);
+
+			$userData = $this->User->login(
+					$userData['username'], $userData['password']
+			);
+
+			// Log them in and refresh page
+			if (!empty($userData) && $this->Auth->login($userData['User'])) {
+				$this->redirect($this->request->here);
+			}
+
+		}
+		parent::beforeFilter();
+	}
 }
