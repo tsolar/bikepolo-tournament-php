9b4661326455c03046f5a6e864aa1122032f84cc
Tomás Solar <tsolar@gmail.com>
2013-08-15 03:33:55 -0400

    Controller/UsersController.php: creating User and Profile when logging with provider

--- Controller/UsersController.php
+++ Controller/UsersController.php
@@ -10,14 +10,64 @@ class UsersController extends AppController {
 
 	//public $scaffold = 'admin';
 	
-	public function opauth_complete() {
-		debug($this->data);
-	}
-
 	public function beforeFilter() {
-		$this->Auth->allow('login', 'logout');
+		$this->Auth->allow('login', 'logout', 'opauth_complete');
 		parent::beforeFilter();
 	}
+
+	public function opauth_complete() {
+		$this->autoLayout = false;
+		$this->autoRender = false;
+
+		$user = $this->User->findById($this->Auth->user('id'));
+		$profile = array();
+		$user_data = array();
+		$profile['user_id'] = null;
+		if(!empty($this->data)) {
+			if(!empty($this->data['auth']['info'])) {
+				$auth_info = $this->data['auth']['info'];
+				$profile['first_name'] = $auth_info['first_name'];
+				$profile['last_name'] = $auth_info['last_name'];
+				$profile['location'] = $auth_info['location'];
+				$profile['image'] = $auth_info['image'];
+			}
+			if(!empty($this->data['auth']['raw'])) {
+				$raw = $this->data['auth']['raw'];
+				$profile['gender'] = $raw['gender'];
+				$profile['timezone'] = $raw['timezone'];
+				$profile['locale'] = $raw['locale'];
+				$user_data['username'] = $raw['username'];
+			}
+		}
+
+		// verifico que si no hay usuario con el id, no haya con el username
+		if(empty($user)) {
+			$user = $this->User->findByUsername($user_data['username']);
+		}
+
+		if(!empty($user)) {
+			$data = $user;
+			$profile['user_id'] = $user['User']['id'];
+			$data['Profile'] = $profile; // cambiar por foreach
+			$data['Profile']['id'] = $user['Profile']['id'];
+		} else {
+			$data = $user;
+			$data['User'] = array();
+			$data['User']['username'] = $user_data['username'];
+			$data['Profile'] = $profile; 
+			
+		}
+		if ($this->User->saveAssociated($data)) {
+			$this->Session->setFlash(__('Account associated!'));
+			$this->Auth->login($data['User']);
+			return $this->redirect(Router::url('/users/profile'));
+		}
+		else {
+			$this->Session->setFlash(__("Can't associate account :("));
+		}
+		return $this->redirect(Router::url('/users/login'));
+	}
+
 	// This allows me to fetch the user row however I want and set a cookie too
 	public function login() {
 		// 2.0 Auth login
@@ -64,5 +114,9 @@ class UsersController extends AppController {
 
 	public function logout() {
 		return $this->redirect($this->Auth->logout());
+	}
+
+	public function profile() {
+		
 	}
 }
