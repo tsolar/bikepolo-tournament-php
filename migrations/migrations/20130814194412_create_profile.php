5fa2efb78239fc4f3a359bf13af37218e5cd5438
Tomás Solar <tsolar@gmail.com>
2013-08-15 03:50:23 -0400

    migrations: create profile user_id->integer

--- migrations/migrations/20130814194412_create_profile.php
+++ migrations/migrations/20130814194412_create_profile.php
@@ -7,7 +7,7 @@ class CreateProfile extends AbstractMigration
     public function change()
     {
 		$table = $this->table('profiles');
-		$table->addColumn('user_id', 'string');
+		$table->addColumn('user_id', 'integer');
 		$table->addColumn('gender', 'string', array('null'=>true, 'default'=>null));
 		$table->addColumn('first_name', 'string', array('null'=>true, 'default'=>null));
 		$table->addColumn('last_name', 'string', array('null'=>true, 'default'=>null));
