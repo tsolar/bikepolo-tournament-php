4b1509ed1e7ed7a057f10a2dc8ec372841ea03ff
Tomás Solar <tsolar@gmail.com>
2013-08-15 03:31:12 -0400

    Model/User.php: email can be empty

--- Model/User.php
+++ Model/User.php
@@ -39,7 +39,7 @@ class User extends AppModel {
 			'email' => array(
 				'rule' => array('email'),
 				//'message' => 'Your custom message here',
-				//'allowEmpty' => false,
+				'allowEmpty' => true,
 				//'required' => false,
 				//'last' => false, // Stop validation after this rule
 				//'on' => 'create', // Limit validation to 'create' or 'update' operations
