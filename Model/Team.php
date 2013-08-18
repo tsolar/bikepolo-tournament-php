d3aa1120500a358fbd7d8aff5f8d0dcc37008c92
Tomás Solar <tsolar@gmail.com>
2013-08-18 01:40:21 -0400

    Model/Team.php: added. After create a team a membership with current user is created

---
+++ Model/Team.php
@@ -0,0 +1,97 @@
+<?php
+App::uses('AppModel', 'Model');
+/**
+ * Team Model
+ *
+ * @property TeamMembership $TeamMembership
+ */
+class Team extends AppModel {
+
+/**
+ * Display field
+ *
+ * @var string
+ */
+	public $displayField = 'name';
+
+
+/**
+ * Validation rules
+ *
+ * @var array
+ */
+	public $validate = array(
+		'name' => array(
+			'notempty' => array(
+				'rule' => array('notempty'),
+				//'message' => 'Your custom message here',
+				//'allowEmpty' => false,
+				//'required' => false,
+				//'last' => false, // Stop validation after this rule
+				//'on' => 'create', // Limit validation to 'create' or 'update' operations
+			),
+		),
+		'since_date' => array(
+			'date' => array(
+				'rule' => array('date'),
+				//'message' => 'Your custom message here',
+				//'allowEmpty' => false,
+				//'required' => false,
+				//'last' => false, // Stop validation after this rule
+				//'on' => 'create', // Limit validation to 'create' or 'update' operations
+			),
+		),
+		'active' => array(
+			'boolean' => array(
+				'rule' => array('boolean'),
+				//'message' => 'Your custom message here',
+				//'allowEmpty' => false,
+				//'required' => false,
+				//'last' => false, // Stop validation after this rule
+				//'on' => 'create', // Limit validation to 'create' or 'update' operations
+			),
+		),
+	);
+	
+	//The Associations below have been created with all possible keys, those that are not needed can be removed
+
+/**
+ * hasMany associations
+ *
+ * @var array
+ */
+	public $hasMany = array(
+		'TeamMembership' => array(
+			'className' => 'TeamMembership',
+			'foreignKey' => 'team_id',
+			'dependent' => false,
+			'conditions' => '',
+			'fields' => '',
+			'order' => '',
+			'limit' => '',
+			'offset' => '',
+			'exclusive' => '',
+			'finderQuery' => '',
+			'counterQuery' => ''
+		)
+	);
+
+	public function afterSave($created) {
+		if($created) {
+			$this->TeamMembership->create();
+			$user = $this->getCurrentUser();
+			if(!empty($user['Player'])) {
+				$data = array(
+					'player_id'=>$user['Player']['id'],
+					'team_id'=>$this->id,
+					'approved'=>1,
+					'is_admin'=>1,
+					'is_captain'=>1,
+					'join_date'=>date('Y-m-d')
+				);
+				$this->TeamMembership->save($data);
+			}
+		}
+		return parent::afterSave($created);
+	}
+}
