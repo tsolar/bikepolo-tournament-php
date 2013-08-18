10d3a39d81ee510cdba7e26b1da7e2ae9e05fb41
Tomás Solar <tsolar@gmail.com>
2013-08-18 01:41:23 -0400

    Model/Player: added.

---
+++ Model/Player.php
@@ -0,0 +1,104 @@
+<?php
+App::uses('AppModel', 'Model');
+/**
+ * Player Model
+ *
+ * @property User $User
+ * @property Team $Team
+ */
+class Player extends AppModel {
+
+/**
+ * Display field
+ *
+ * @var string
+ */
+	public $displayField = 'name';
+
+/**
+ * Validation rules
+ *
+ * @var array
+ */
+	public $validate = array(
+		'name' => array(
+			'notempty' => array(
+				'rule' => array('notempty'),
+				//'message' => 'Your custom message here',
+				//'allowEmpty' => false,
+				//'required' => false,
+				//'last' => false, // Stop validation after this rule
+				//'on' => 'create', // Limit validation to 'create' or 'update' operations
+			),
+		),
+		'since_date' => array(
+			'date' => array(
+				'rule' => array('date'),
+				//'message' => 'Your custom message here',
+				//'allowEmpty' => false,
+				//'required' => false,
+				//'last' => false, // Stop validation after this rule
+				//'on' => 'create', // Limit validation to 'create' or 'update' operations
+			),
+		),
+		'active' => array(
+			'boolean' => array(
+				'rule' => array('boolean'),
+				//'message' => 'Your custom message here',
+				//'allowEmpty' => false,
+				//'required' => false,
+				//'last' => false, // Stop validation after this rule
+				//'on' => 'create', // Limit validation to 'create' or 'update' operations
+			),
+		),
+	);
+
+	//The Associations below have been created with all possible keys, those that are not needed can be removed
+
+/**
+ * belongsTo associations
+ *
+ * @var array
+ */
+	public $belongsTo = array(
+		'User' => array(
+			'className' => 'User',
+			'foreignKey' => 'user_id',
+			'conditions' => '',
+			'fields' => '',
+			'order' => ''
+		)
+	);
+
+/**
+ * hasMany associations
+ *
+ * @var array
+ */
+	public $hasMany = array(
+		'TeamMembership' => array(
+			'className' => 'TeamMembership',
+			'foreignKey' => 'player_id',
+			'dependent' => false,
+			'conditions' => '',
+			'fields' => '',
+			'order' => '',
+			'limit' => '',
+			'offset' => '',
+			'exclusive' => '',
+			'finderQuery' => '',
+			'counterQuery' => ''
+		)
+	);
+
+	public function findWithoutCurrentUser($type = 'first', $params = array()) {
+		return $this->findWithoutMe($type, $params);
+	}
+	public function findWithoutMe($type = 'first', $params = array()) {
+		$user = $this->getCurrentUser();
+		debug($user);
+		$user_id = $user['id'];
+		$params['conditions']['user_id !='] = $user_id;
+		return $this->find($type, $params);
+	}
+}
