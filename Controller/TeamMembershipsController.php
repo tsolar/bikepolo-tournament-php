ff9cd2611d0bf2a3e4e24269182b4cc50706fb13
Tomás Solar <tsolar@gmail.com>
2013-08-18 01:42:12 -0400

    Controller: Teams, Players and TeamMemberships controllers added

---
+++ Controller/TeamMembershipsController.php
@@ -0,0 +1,16 @@
+<?php
+App::uses('AppController', 'Controller');
+/**
+ * PlayersTeams Controller
+ *
+ */
+class TeamMembershipsController extends AppController {
+
+/**
+ * Scaffold
+ *
+ * @var mixed
+ */
+	public $scaffold;
+
+}
---
+++ Controller/TeamsController.php
@@ -0,0 +1,183 @@
+<?php
+App::uses('AppController', 'Controller');
+/**
+ * Teams Controller
+ *
+ * @property Team $Team
+ */
+class TeamsController extends AppController {
+
+/**
+ * index method
+ *
+ * @return void
+ */
+	public function index() {
+		$this->Team->recursive = 0;
+		$this->set('teams', $this->paginate());
+	}
+
+/**
+ * view method
+ *
+ * @throws NotFoundException
+ * @param string $id
+ * @return void
+ */
+	public function view($id = null) {
+		if (!$this->Team->exists($id)) {
+			throw new NotFoundException(__('Invalid team'));
+		}
+		$options = array('conditions' => array('Team.' . $this->Team->primaryKey => $id));
+		$this->set('team', $this->Team->find('first', $options));
+	}
+
+/**
+ * add method
+ *
+ * @return void
+ */
+	public function add() {
+		if ($this->request->is('post')) {
+			$this->Team->create();
+			if ($this->Team->save($this->request->data)) {
+				$this->Session->setFlash(__('The team has been saved'));
+				$this->redirect(array('action' => 'index'));
+			} else {
+				$this->Session->setFlash(__('The team could not be saved. Please, try again.'));
+			}
+		}
+	}
+
+/**
+ * edit method
+ *
+ * @throws NotFoundException
+ * @param string $id
+ * @return void
+ */
+	public function edit($id = null) {
+		if (!$this->Team->exists($id)) {
+			throw new NotFoundException(__('Invalid team'));
+		}
+		if ($this->request->is('post') || $this->request->is('put')) {
+			if ($this->Team->save($this->request->data)) {
+				$this->Session->setFlash(__('The team has been saved'));
+				$this->redirect(array('action' => 'index'));
+			} else {
+				$this->Session->setFlash(__('The team could not be saved. Please, try again.'));
+			}
+		} else {
+			$options = array('conditions' => array('Team.' . $this->Team->primaryKey => $id));
+			$this->request->data = $this->Team->find('first', $options);
+		}
+	}
+
+/**
+ * delete method
+ *
+ * @throws NotFoundException
+ * @param string $id
+ * @return void
+ */
+	public function delete($id = null) {
+		$this->Team->id = $id;
+		if (!$this->Team->exists()) {
+			throw new NotFoundException(__('Invalid team'));
+		}
+		$this->request->onlyAllow('post', 'delete');
+		if ($this->Team->delete()) {
+			$this->Session->setFlash(__('Team deleted'));
+			$this->redirect(array('action' => 'index'));
+		}
+		$this->Session->setFlash(__('Team was not deleted'));
+		$this->redirect(array('action' => 'index'));
+	}
+
+/**
+ * admin_index method
+ *
+ * @return void
+ */
+	public function admin_index() {
+		$this->Team->recursive = 0;
+		$this->set('teams', $this->paginate());
+	}
+
+/**
+ * admin_view method
+ *
+ * @throws NotFoundException
+ * @param string $id
+ * @return void
+ */
+	public function admin_view($id = null) {
+		if (!$this->Team->exists($id)) {
+			throw new NotFoundException(__('Invalid team'));
+		}
+		$options = array('conditions' => array('Team.' . $this->Team->primaryKey => $id));
+		$this->set('team', $this->Team->find('first', $options));
+	}
+
+/**
+ * admin_add method
+ *
+ * @return void
+ */
+	public function admin_add() {
+		if ($this->request->is('post')) {
+			$this->Team->create();
+			if ($this->Team->save($this->request->data)) {
+				$this->Session->setFlash(__('The team has been saved'));
+				$this->redirect(array('action' => 'index'));
+			} else {
+				$this->Session->setFlash(__('The team could not be saved. Please, try again.'));
+			}
+		}
+	}
+
+/**
+ * admin_edit method
+ *
+ * @throws NotFoundException
+ * @param string $id
+ * @return void
+ */
+	public function admin_edit($id = null) {
+		if (!$this->Team->exists($id)) {
+			throw new NotFoundException(__('Invalid team'));
+		}
+		if ($this->request->is('post') || $this->request->is('put')) {
+			if ($this->Team->save($this->request->data)) {
+				$this->Session->setFlash(__('The team has been saved'));
+				$this->redirect(array('action' => 'index'));
+			} else {
+				$this->Session->setFlash(__('The team could not be saved. Please, try again.'));
+			}
+		} else {
+			$options = array('conditions' => array('Team.' . $this->Team->primaryKey => $id));
+			$this->request->data = $this->Team->find('first', $options);
+		}
+	}
+
+/**
+ * admin_delete method
+ *
+ * @throws NotFoundException
+ * @param string $id
+ * @return void
+ */
+	public function admin_delete($id = null) {
+		$this->Team->id = $id;
+		if (!$this->Team->exists()) {
+			throw new NotFoundException(__('Invalid team'));
+		}
+		$this->request->onlyAllow('post', 'delete');
+		if ($this->Team->delete()) {
+			$this->Session->setFlash(__('Team deleted'));
+			$this->redirect(array('action' => 'index'));
+		}
+		$this->Session->setFlash(__('Team was not deleted'));
+		$this->redirect(array('action' => 'index'));
+	}
+}
