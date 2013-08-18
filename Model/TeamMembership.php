7271c170b7c9a50797667e174a8c73dfe966c21b
Tomás Solar <tsolar@gmail.com>
2013-08-18 01:39:29 -0400

    Model/TeamMembership.php: created model to associate Teams with Players

---
+++ Model/TeamMembership.php
@@ -0,0 +1,44 @@
+<?php
+App::uses('AppModel', 'Model');
+/**
+ * PlayersTeam Model
+ *
+ * @property Player $Player
+ * @property Team $Team
+ */
+class TeamMembership extends AppModel {
+
+	public $useTable = 'players_teams';
+
+/**
+ * Display field
+ *
+ * @var string
+ */
+	public $displayField = 'id';
+
+
+	//The Associations below have been created with all possible keys, those that are not needed can be removed
+
+/**
+ * belongsTo associations
+ *
+ * @var array
+ */
+	public $belongsTo = array(
+		'Player' => array(
+			'className' => 'Player',
+			'foreignKey' => 'player_id',
+			'conditions' => '',
+			'fields' => '',
+			'order' => ''
+		),
+		'Team' => array(
+			'className' => 'Team',
+			'foreignKey' => 'team_id',
+			'conditions' => '',
+			'fields' => '',
+			'order' => ''
+		)
+	);
+}
