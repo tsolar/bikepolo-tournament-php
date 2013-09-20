aeb78bc2edb7d0f725fee78472625f1555d0ca06
tsolar <tsolar@gmail.com>
2013-09-19 22:43:38 -0300

    Config/routes.php: added Users routes

--- Config/routes.php
+++ Config/routes.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Routes configuration
  *
@@ -12,34 +13,41 @@
  * @package       app.Config
  * @since         CakePHP(tm) v 0.2.9
  */
-
 /**
  * Here, we are connecting '/' (base path) to controller called 'Pages',
  * its action called 'display', and we pass a param to select the view file
  * to use (in this case, /app/View/Pages/home.ctp)...
  */
-	Router::connect('/', array('controller' => 'pages', 'action' => 'display', 'home'));
+Router::connect('/', array('controller' => 'pages', 'action' => 'display', 'home'));
 /**
  * ...and connect the rest of 'Pages' controller's URLs.
  */
-	Router::connect('/pages/*', array('controller' => 'pages', 'action' => 'display'));
+Router::connect('/pages/*', array('controller' => 'pages', 'action' => 'display'));
 
 
-	Router::connect(
-		'/opauth-complete/*',
-		array('controller' => 'users', 'action' => 'opauth_complete')
-	);
+Router::connect(
+		'/opauth-complete/*', array('controller' => 'users', 'action' => 'opauth_complete')
+);
+/**
+ * users plugin
+ */
+Router::connect('/users', array('plugin' => null, 'controller' => 'app_users'));
+Router::connect('/users/index/*', array('plugin' => null, 'controller' => 'app_users'));
+Router::connect('/users/users/:action/*', array('plugin' => null, 'controller' => 'app_users'));
+Router::connect('/users/:action/*', array('plugin' => null, 'controller' => 'app_users'));
+Router::connect('/admin/users/login', array('plugin' => null, 'controller' => 'app_users', 'action' => 'login'));
+Router::connect('/login/*', array('plugin' => null, 'controller' => 'app_users', 'action' => 'login'));
+Router::connect('/logout/*', array('plugin' => null, 'controller' => 'app_users', 'action' => 'logout'));
+Router::connect('/register/*', array('plugin' => null, 'controller' => 'app_users', 'action' => 'add'));
 
-	Router::connect('/login', array('controller' => 'users', 'action' => 'login'));
-	Router::connect('/logout', array('controller' => 'users', 'action' => 'logout'));
 /**
  * Load all plugin routes. See the CakePlugin documentation on
  * how to customize the loading of plugin routes.
  */
-	CakePlugin::routes();
+CakePlugin::routes();
 
 /**
  * Load the CakePHP default routes. Only remove this if you do not want to use
  * the built-in default routes.
  */
-	require CAKE . 'Config' . DS . 'routes.php';
+require CAKE . 'Config' . DS . 'routes.php';
