3f0c71ca7589b806cf542df45297665c62f5fe0c
tsolar <tsolar@gmail.com>
2013-09-27 01:18:34 -0300

    Model/AppUser.php: added Player in afterSave()

--- Model/AppUser.php
+++ Model/AppUser.php
@@ -2,6 +2,12 @@
 
 App::uses('User', 'Users.Model');
 
+/**
+ * @property Player $Player
+ * @property Profile $Profile
+ * @property SocialAccount $SocialAccount
+ * @property UserDetail $UserDetails
+ */
 class AppUser extends User {
 
 	public $useTable = 'users';
@@ -53,5 +59,25 @@ class AppUser extends User {
 			'foreignKey' => 'user_id'
 		),
 	);
+
+	public function afterSave($created, $options = array()) {
+		if ($created) {
+			if (empty($this->data['Player'])) {
+				$this->data['Player']['since_date'] = date('Y-m-d');
+				$this->data['Player']['active'] = true;
+				$this->data['Player']['name'] = $this->data['AppUser']['username'] ? $this->data['AppUser']['username'] : 'NN';
+				$this->data['Player']['user_id'] = $this->data['AppUser']['id'];
+				$player = $this->Player->save($this->data['Player']);
+				if (!empty($player)) {
+					//return true;
+				}
+			}
+		}
+		parent::afterSave($created, $options);
+	}
+
+	public function beforeSave($options = array()) {
+		
+	}
 
 }
