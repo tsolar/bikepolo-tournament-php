c45d574960873f2f5afd6f127013e03f7b4cda33
tsolar <tsolar@gmail.com>
2013-10-02 06:23:42 -0300

    Config/routes.php: admin users routes :@

--- Config/routes.php
+++ Config/routes.php
@@ -34,9 +34,9 @@ Router::connect(
 Router::connect('/users', array('plugin' => null, 'controller' => 'app_users'));
 Router::connect('/users/index/*', array('plugin' => null, 'controller' => 'app_users'));
 //admin
-Router::connect('/admin/users/*', array('plugin' => null, 'controller' => 'app_users', 'action' => 'index', 'prefix' => 'admin'));
-Router::connect('/admin/users/users/:action/*', array('plugin' => null, 'controller' => 'app_users', 'prefix' => 'admin'));
-Router::connect('/admin/users/:action/*', array('plugin' => null, 'controller' => 'app_users', 'prefix' => 'admin'));
+//Router::connect('/admin/users/*', array('plugin' => null, 'controller' => 'app_users', 'action' => 'index', 'prefix' => 'admin'));
+//Router::connect('/admin/users/users/:action/*', array('plugin' => null, 'controller' => 'app_users', 'prefix' => 'admin'));
+//Router::connect('/admin/users/:action/*', array('plugin' => null, 'controller' => 'app_users', 'prefix' => 'admin'));
 
 Router::connect('/users/users/:action/*', array('plugin' => null, 'controller' => 'app_users'));
 Router::connect('/users/:action/*', array('plugin' => null, 'controller' => 'app_users'));
